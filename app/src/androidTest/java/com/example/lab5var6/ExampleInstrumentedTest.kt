c9c278f76e77e765189862753dc6c8020ffeb985
Denis <bogusevich03@gmail.com>
2024-05-26 15:27:21 +0300

    tests

--- app/src/androidTest/java/com/example/lab5var6/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lab5var6/ExampleInstrumentedTest.kt
@@ -1,12 +1,17 @@
 package com.example.lab5var6
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Assert
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +19,72 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.lab5var6", appContext.packageName)
+class EarthquakeDatabaseTest {
+    private lateinit var dbHelper: EarthquakeDatabaseHelper
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        dbHelper = EarthquakeDatabaseHelper(context)
     }
+
+    @After
+    fun tearDown() {
+        dbHelper.clearAllEarthquakes()
+    }
+
+    @Test
+    fun insertBook_success() {
+        val earthquakeProperties =EarthquakeProperties(
+            "test",10)
+
+
+
+        dbHelper.insertEarthquake(earthquakeProperties)
+        val earthquakes = dbHelper.getAllEarthquakes()
+
+
+
+        assertEquals(earthquakeProperties.place, earthquakes[0].place)
+        assertEquals(earthquakeProperties.time, earthquakes[0].time)
+
+
+    }
+
+    @Test(expected = Exception::class)
+    fun insertBook_fail() {
+        val earthquakeProperties =EarthquakeProperties(
+            "test",10)
+
+        dbHelper.insertEarthquake(earthquakeProperties)
+        dbHelper.insertEarthquake(earthquakeProperties)
+
+    }
+
+
+    @Test
+    fun clearAllBooks_success() {
+        val earthquakeProperties =EarthquakeProperties(
+            "test",10)
+
+        val earthquakeProperties1 =EarthquakeProperties(
+            "test2",15)
+
+        dbHelper.insertEarthquake(earthquakeProperties)
+        dbHelper.insertEarthquake(earthquakeProperties1)
+        dbHelper.clearAllEarthquakes()
+        val earthquakes = dbHelper.getAllEarthquakes()
+        Assert.assertTrue(earthquakes.isEmpty())
+    }
+
+
+
+    @Test
+    fun testGetAllBooks_emptyDatabase() {
+        // Ensure the database is empty
+        dbHelper.clearAllEarthquakes()
+        val earthquakes = dbHelper.getAllEarthquakes()
+        Assert.assertTrue(earthquakes.isEmpty())
+    }
+
 }
--- app/src/test/java/com/example/lab5var6/ExampleUnitTest.kt
+++ app/src/test/java/com/example/lab5var6/ExampleUnitTest.kt
@@ -1,17 +1,18 @@
 package com.example.lab5var6
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-}
