75e8cec8e0ec2cd66d5f24a65c7606226b67aea5
Denis <bogusevich03@gmail.com>
2024-05-26 17:53:17 +0300

    6

--- app/src/androidTest/java/com/example/lab5var6/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lab5var6/ExampleInstrumentedTest.kt
@@ -1,24 +1,85 @@
 package com.example.lab5var6
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Assert
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.lab5var6", appContext.packageName)
+class EarthquakeDatabaseTest {
+    private lateinit var dbHelper: EarthquakeDb
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        dbHelper = EarthquakeDb(context)
     }
+
+    @After
+    fun tearDown() {
+        dbHelper.clearAllEarthquakes()
+    }
+
+    @Test
+    fun insertBook_success() {
+        val earthquake =Earthquake(
+            "test",10)
+
+
+
+        dbHelper.insertEarthquake(earthquake)
+        val earthquakes = dbHelper.getAllEarthquakes()
+
+
+
+        assertEquals(earthquake.place, earthquakes[0].place)
+        assertEquals(earthquake.time, earthquakes[0].time)
+
+
+    }
+
+    @Test(expected = Exception::class)
+    fun insertBook_fail() {
+        val earthquake =Earthquake(
+            "test",10)
+
+        dbHelper.insertEarthquake(earthquake)
+        dbHelper.insertEarthquake(earthquake)
+
+    }
+
+
+    @Test
+    fun clearAllBooks_success() {
+        val earthquake =Earthquake(
+            "test",10)
+
+        val earthquake1 =Earthquake(
+            "test2",15)
+
+        dbHelper.insertEarthquake(earthquake)
+        dbHelper.insertEarthquake(earthquake1)
+        dbHelper.clearAllEarthquakes()
+        val earthquakes = dbHelper.getAllEarthquakes()
+        assertTrue(earthquakes.isEmpty())
+    }
+
+
+
+    @Test
+    fun testGetAllBooks_emptyDatabase() {
+
+        dbHelper.clearAllEarthquakes()
+        val earthquakes = dbHelper.getAllEarthquakes()
+        assertTrue(earthquakes.isEmpty())
+    }
+
 }
